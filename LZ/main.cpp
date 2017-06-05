3b9dea21daed753e9dc71fa255dee67e4bf95280
iFlake <iceflake@cloudwards.es>
2017-06-05 12:48:35 +0400

    Testing performance

--- LZ/main.cpp
+++ LZ/main.cpp
@@ -18,21 +18,44 @@ int main()
 
 	LZ::LZvm vm;
 
-	std::vector<int> references;
+	std::vector<LZ::LZref> references;
+
+	for (int i = 0; i < 10000; i++)
+	{
+		int a = 25;
+		a = a * a;
+		a = a / a;
+	}
+
+	std::cout << "Warm up complete\n";
 
 	clock_t c = clock();
 
-	for (int i = 0; i < 1000; ++i)
+	for (int i = 0; i < 10000; ++i)
 	{
 		LZ::LZobject* object = new LZ::LZobject;
-		//vm.Allocate(object);
+		LZ::LZref r = vm.Allocate(object);
+		references.push_back(r);
 	}
 
 	clock_t c2 = clock();
 
 	unsigned long long timetaken = c2 - c;
 
-	std::cout << timetaken;
+	std::cout << timetaken; std::cout << "\n";
+
+	clock_t c3 = clock();
+
+	for (int i = 0; i < references.size(); ++i)
+	{
+		vm.Deallocate(references.at(i));
+	}
+
+	clock_t c4 = clock();
+
+	unsigned long long timetaken2 = c4 - c3;
+
+	std::cout << timetaken2; std::cout << "\n";
 
 	std::string xx;
 	std::cin >> xx;
